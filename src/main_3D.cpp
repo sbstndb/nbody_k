99e049020b63f62e0d247f6aebceb4afeb179c0a
sbstndb <30315405+sbstndb@users.noreply.github.com>
2024-08-10 23:00:44 +0200

    Add new Features
    	-	Resolve an issue on random distribution for initial
    		positions
    	-	Add New CLI arguments

--- src/main_3D.cpp
+++ src/main_3D.cpp
@@ -6,12 +6,27 @@
 #include <cstring>
 
 
+#include <fstream>
+#include <string>
+#include <iomanip> // Pour formater la sortie numérique
+
+#include <random>
+
 #include <Kokkos_Core.hpp>
 
 
 const double dt = 0.01 ; 
 const double G = 1.0;
 
+// to share with devices!!
+struct Setting {
+	double G = 1.0 ; 
+	double dt = 0.01;
+	int N = 1024 ; // number of bodies
+	int nrepeat = 100 ; // number of repeats of the test
+	int frames = 100;  // number of saved frames. Each frame execute nrepeat iterations !
+
+};
 
 struct Particle {
 	double x, y, z;
@@ -21,24 +36,54 @@ struct Particle {
 };
 
 
+void save_particles_to_file(const Kokkos::View<Particle*>::HostMirror particles, int n,  int frame_number) {
+    std::string filename = "frames/frame_" + std::to_string(frame_number) + ".txt";
+
+    std::ofstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Erreur lors de l'ouverture du fichier : " << filename << std::endl;
+        return;
+    }
+
+    // Enregistrer les particules dans le fichier
+    for (int i = 0 ; i < n ; i++) {
+        file << particles[i].x << " " << particles[i].y << " " << particles[i].z << " "
+             << particles[i].vx << " " << particles[i].vy << " " << particles[i].vz << " "
+             << particles[i].fx << " " << particles[i].fy << " " << particles[i].fz << " "
+             << particles[i].mass << "\n";
+    }
+    file.close();
+}
+
+
 int main( int argc, char* argv[] )
 {
-  int N = 1024; // number off bodies
-  int nrepeat = 100;  // number of repeats of the test
-
+  
+  Setting setting;
 
   // Read command line arguments.
   for ( int i = 0; i < argc; i++ ) {
     if ( ( strcmp( argv[ i ], "-n" ) == 0 ) || ( strcmp( argv[ i ], "-N" ) == 0 ) ) {
-      N = atoi( argv[++i]);
-      printf( "  User N is %d\n", N );
+      setting.N = atoi( argv[++i]);
+      printf( "  User N is %d\n", setting.N );
+    }
+    else if ( ( strcmp( argv[ i ], "-f" ) == 0 ) || ( strcmp( argv[ i ], "-F" ) == 0 ) ) {
+      setting.frames = atoi( argv[++i]);
+      printf( "  User frames is %d\n", setting.frames );
+    }
+    else if ( ( strcmp( argv[ i ], "-d" ) == 0 ) || ( strcmp( argv[ i ], "-D" ) == 0 ) ) {
+      setting.dt = atof( argv[++i]);
+      printf( "  User dt is %d\n", setting.dt );
     }
     else if ( strcmp( argv[ i ], "-nrepeat" ) == 0 ) {
-      nrepeat = atoi( argv[ ++i ] );
+      setting.nrepeat = atoi( argv[ ++i ] );
     }
     else if ( ( strcmp( argv[ i ], "-h" ) == 0 ) || ( strcmp( argv[ i ], "-help" ) == 0 ) ) {
       printf( "  NBody Options:\n" );
       printf( "  -n (-N) <int>:         number of particles\n" );
+      printf( "  -f (-F) <int>:         number of saved frames\n" );      
+      printf( "  -d (-D) <int>:         time step value\n" );
       printf( "  -nrepeat <int>:        number of repetitions (default: 100)\n" );
       printf( "  -help (-h):            print this message\n\n" );
       exit( 1 );
@@ -46,20 +91,33 @@ int main( int argc, char* argv[] )
   }
 
 
+
   Kokkos::initialize( argc, argv );
   {
 
   // Allocate y, x vectors and Matrix A on device.
+  typedef Kokkos::View<Setting*> ViewSetting ; 
   typedef Kokkos::View<Particle*>   ViewVectorType;
-  ViewVectorType p("p", N);
+  ViewSetting sv("setting", 1);
+  ViewVectorType p("p", setting.N);
 
   // Create host mirrors of device views.
     ViewVectorType::HostMirror h_p = Kokkos::create_mirror_view( p ) ;
+    ViewSetting::HostMirror h_sv = Kokkos::create_mirror_view( sv) ;
+  h_sv[0] = setting ;     
 
-  Kokkos::parallel_for("init", N, KOKKOS_LAMBDA (int i){
-	h_p(i).x = rand() / (double)RAND_MAX;
-	h_p(i).y = rand() / (double)RAND_MAX;
-	h_p(i).z = rand() / (double)RAND_MAX;
+   std::random_device rd;  // Pour obtenir une graine unique
+   std::mt19937 gen(rd()); // Mersenne Twister engine seeded with rd()
+   std::uniform_real_distribution<double> dis(0.0, 1.0);
+
+       std::cout << "value : " << dis(gen) << std::endl ;
+
+       h_p(0).x = dis(gen) ; 
+
+  Kokkos::parallel_for("init", Kokkos::RangePolicy<Kokkos::HostSpace::execution_space>(0, setting.N), KOKKOS_LAMBDA (int i){
+	h_p(i).x = 0.0;
+	h_p(i).y = 0.0;
+	h_p(i).z = 0.0;
 	h_p(i).vx = 0.0 ;
 	h_p(i).vy = 0.0 ; 
 	h_p(i).vz = 0.0 ;
@@ -68,44 +126,66 @@ int main( int argc, char* argv[] )
         h_p(i).fz = 0.0 ;
 	h_p(i).mass = 1.0;
   });
+// i dont know why but i cant atm use dis(gen) in the kokkos kernel in host space 
+  for (int i = 0 ; i < setting.N ; i++){
+	  h_p(i).x = dis(gen);
+	  h_p(i).y = dis(gen);
+	  h_p(i).z = dis(gen);
+  }
 
   // Deep copy host views to device views.
     Kokkos::deep_copy(p, h_p);
+    Kokkos::deep_copy(sv, h_sv);
+
 
   // Timer products.
   Kokkos::Timer timer;
+for ( int frame = 0 ; frame < h_sv(0).frames; frame++){
+  for ( int repeat = 0; repeat < h_sv(0).nrepeat; repeat++ ) {
 
-  for ( int repeat = 0; repeat < nrepeat; repeat++ ) {
-
-      Kokkos::parallel_for("kernel", N, KOKKOS_LAMBDA (int i){
+      Kokkos::parallel_for("kernel", h_sv(0).N, KOKKOS_LAMBDA (int i){
       double dx , dy , dz;
       double dist, dist3, force;
       p(i).fx = 0.0 ; 
       p(i).fy = 0.0 ; 
       p(i).fz = 0.0 ; 
-      for (int j = 0 ; j < N ; j++){
+   
+      double eps = 1e-10;
+      for (int j = 0 ; j < sv(0).N ; j++){
+      	if (i==j)
+		continue;
 	dx = p(j).x - p(i).x ;
+
 	dy = p(j).y - p(i).y ; 
         dz = p(j).z - p(i).z ;
 	dist = sqrt(dx*dx + dy*dy + dz*dz);
-	dist3 = dist * dist * dist;
-	force = G * p(j).mass / dist3 ; 
+	dist3 = dist * dist * dist + eps;
+	force = sv(0).G * p(j).mass / dist3 ; 
 	p(i).fx += force * dx ; 
 	p(i).fy += force * dy ;
 	p(i).fz += force * dz ;
+
       }
       });
-      Kokkos::parallel_for("kernel2", N, KOKKOS_LAMBDA (int i){
+      Kokkos::parallel_for("kernel2", h_sv(0).N, KOKKOS_LAMBDA (int i){
         // integration 
 	double mass = p(i).mass;
-        p(i).vx += dt * p(i).fx / mass ;
-        p(i).vy += dt * p(i).fy / mass ;
-        p(i).vz += dt * p(i).fz / mass ;
+        p(i).vx += sv(0).dt * p(i).fx / mass ;
+        p(i).vy += sv(0).dt * p(i).fy / mass ;
+        p(i).vz += sv(0).dt * p(i).fz / mass ;
 	// integration 2
-        p(i).x += p(i).vx * dt ;
-        p(i).y += p(i).vy * dt ;
-        p(i).z += p(i).vz * dt ;
+        p(i).x += p(i).vx * sv(0).dt ;
+        p(i).y += p(i).vy * sv(0).dt ;
+        p(i).z += p(i).vz * sv(0).dt;
         });
+
+  // here we want to save values in file .txt
+  // --> deepcopy to host
+  // --> and save in file in folder "result" 
+  }
+  // save the frame
+  Kokkos:deep_copy(h_p, p) ; 
+  save_particles_to_file(h_p, h_sv(0).N, frame) ;
   }
 // now i purpose a hierarchical parallelism point of view
 // we have nested loops (for i for j) but the integration is only for i 
